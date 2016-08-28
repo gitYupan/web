2f7da831d462e0d0c29084f3f5f8877d532897f5
gitYupan <981086274@qq.com>
2016-08-28 15:22:41 +0800

    加入shiro 权限管理

--- src/main/java/com/yihou/controller/WebUrlContoller.java
+++ src/main/java/com/yihou/controller/WebUrlContoller.java
@@ -1,5 +1,7 @@
 package com.yihou.controller;
 
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.subject.Subject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -17,6 +19,18 @@ public class WebUrlContoller {
     @RequestMapping(value = {"/", "/;.*"})
     public String index() {
         return "/index";
+    }
+
+    @RequestMapping(value = {"/{login:login;?.*}"})
+    public String login() {
+        //如果用户直接到登录页面 先退出一下
+        //原因：isAccessAllowed实现是subject.isAuthenticated()---->即如果用户验证通过 就允许访问
+        // 这样会导致登录一直死循环
+        Subject subject = SecurityUtils.getSubject();
+        if (subject != null && subject.isAuthenticated()) {
+            subject.logout();
+        }
+        return "/login";
     }
 
 }
---
+++ src/main/webapp/WEB-INF/view/login.jsp
@@ -0,0 +1,15 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>这是登录页面</title>
+</head>
+<body>
+sdfsdaf
+
+<pagejs>
+    <script type="text/javascript">
+        console.info("123");
+    </script>
+</pagejs>
+</body>
+</html>
