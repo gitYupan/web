cce94404b58b98631821b7c9bad91c3c749b6049
gitYupan <981086274@qq.com>
2016-09-21 13:57:54 +0800

    密码加密

--- src/test/java/com/yihou/dao/test/ResourceMapperTest.java
+++ src/test/java/com/yihou/dao/test/ResourceMapperTest.java
@@ -1,6 +1,11 @@
 package com.yihou.dao.test;
 
+import com.alibaba.fastjson.JSON;
+import com.yihou.dao.CoreUserMapper;
+import com.yihou.dao.entity.CoreUserExample;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -8,6 +13,15 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @ContextConfiguration(locations = {"classpath:spring/spring.xml"})
 public class ResourceMapperTest {
 
+    @Autowired
+    public CoreUserMapper coreUserMapper;
+
+    @Test
+    public void testUser() {
+        CoreUserExample coreUserExample = new CoreUserExample();
+        coreUserExample.createCriteria().andUserIdEqualTo(1L);
+        System.out.println(JSON.toJSONString(coreUserMapper.selectByExample(coreUserExample)));
+    }
 
 
 }
